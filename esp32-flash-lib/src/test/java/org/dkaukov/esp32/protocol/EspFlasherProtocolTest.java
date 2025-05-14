6b0aa598e1901f274e82ec6278638d359ed0e810
Dmitry Kaukov <dkaukov@opentable.com>
2025-05-14 19:37:35 +1000

    integration tests

--- esp32-flash-lib/src/test/java/org/dkaukov/esp32/protocol/EspFlasherProtocolTest.java
+++ esp32-flash-lib/src/test/java/org/dkaukov/esp32/protocol/EspFlasherProtocolTest.java
@@ -11,8 +11,112 @@
  */
 package org.dkaukov.esp32.protocol;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.io.IOException;
+import java.nio.file.Path;
+
+import com.fazecast.jSerialComm.SerialPort;
+
+import org.dkaukov.esp32.core.EspFlasherApi;
+import org.dkaukov.esp32.io.SerialTransport;
+import org.dkaukov.esp32.test.SlipLoggingSerialTransport;
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
 
 class EspFlasherProtocolTest {
 
+  static SerialPort comPort;
+  private EspFlasherProtocol protocol;
+
+  static class TestTransport implements SerialTransport {
+    private final SerialPort port;
+    TestTransport(SerialPort port) {
+      this.port = port;
+    }
+
+    @Override
+    public int read(byte[] buffer, int length) {
+      return port.readBytes(buffer, length);
+    }
+
+    @Override
+    public void write(byte[] buffer, int length) {
+      port.writeBytes(buffer, length);
+    }
+
+    @Override
+    public void setControlLines(boolean dtr, boolean rts) {
+      if (dtr) {
+        port.setDTR();
+      }
+      if (rts) {
+        port.setRTS();
+      }
+      if (!dtr) {
+        port.clearDTR();
+      }
+      if (!rts) {
+        port.clearRTS();
+      }
+    }
+  }
+
+  @BeforeAll
+  static void setUp() throws IOException {
+    // get the first port available, you might want to change that
+    comPort = SerialPort.getCommPorts()[6];
+    System.out.println("Connected to: \"" + comPort.getDescriptivePortName() + "\"");
+    comPort.setBaudRate(EspFlasherApi.ESP_ROM_BAUD);
+    comPort.openPort();
+  }
+
+  @Test
+  void sync() throws IOException {
+    protocol = new EspFlasherProtocol(new SlipLoggingSerialTransport(new TestTransport(comPort), Path.of("src/test/resources/sync.txt")));
+    protocol.enterBootLoader();
+    protocol.sync();
+    protocol.reset();
+  }
+
+  @Test
+  void detectChip() throws IOException {
+    protocol = new EspFlasherProtocol(new SlipLoggingSerialTransport(new TestTransport(comPort), Path.of("src/test/resources/detect-chip.txt")));
+    protocol.enterBootLoader();
+    protocol.sync();
+    protocol.detectChip();
+    protocol.reset();
+  }
+
+  @Test
+  void loadStub() throws IOException {
+    protocol = new EspFlasherProtocol(new SlipLoggingSerialTransport(new TestTransport(comPort), Path.of("src/test/resources/load-stub.txt")));
+    protocol.enterBootLoader();
+    protocol.sync();
+    protocol.detectChip();
+    protocol.loadStub();
+    protocol.reset();
+  }
+
+  @Test
+  void writeFlash() throws IOException {
+    byte [] data = new byte[1024];
+    protocol = new EspFlasherProtocol(new SlipLoggingSerialTransport(new TestTransport(comPort), Path.of("src/test/resources/write-flash.txt")));
+    protocol.enterBootLoader();
+    protocol.sync();
+    protocol.detectChip();
+    protocol.loadStub();
+    protocol.flashWrite(data, 0x400, 0x0000);
+    protocol.reset();
+  }
+
+  @Test
+  void eraseFlash() throws IOException {
+    byte [] data = new byte[1024];
+    protocol = new EspFlasherProtocol(new SlipLoggingSerialTransport(new TestTransport(comPort), Path.of("src/test/resources/erase-flash.txt")));
+    protocol.enterBootLoader();
+    protocol.sync();
+    protocol.detectChip();
+    protocol.loadStub();
+    protocol.eraseFlash();
+    protocol.reset();
+  }
 }
